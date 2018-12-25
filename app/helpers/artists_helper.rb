b38ba0cebbbc9131bb4d8ba35f7cdd9dba7d0904
Peter Kreppein <pmkreppein@gmail.com>
2018-12-25 03:21:58 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,6 +4,10 @@ module ArtistsHelper
   end
   
     def artist_select(artist, song)
-
+    if artist
+      artist.name
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name), {include_blank: true}
+    end
   end
 end
